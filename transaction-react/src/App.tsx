740fa68119705365e8bbc31fa33d01e328dd2ab5
Semen Loktionov <loktionov.kh@gmail.com>
2023-03-24 15:15:21 +0200

    fix

--- transaction-react/src/App.tsx
+++ transaction-react/src/App.tsx
@@ -1,62 +1,75 @@
-import { useRef } from 'react'
-import { ethers } from "ethers"
+import {ethers} from "ethers"
 
-import { Price, Target } from '@rarimo/nft-checkout'
-// @ts-ignore
-import { RarimoPayButton, DappContextProvider } from '@rarimo/react-nft-checkout'
-import { useProvider } from '@rarimo/react-provider'
-import { MetamaskProvider } from '@rarimo/provider'
+import {Price} from '@rarimo/nft-checkout'
+import {RarimoPayButton, DappContextProvider} from '@rarimo/react-nft-checkout'
+import {useProvider} from '@rarimo/react-provider'
+import {MetamaskProvider} from '@rarimo/provider'
+import {useEffect, useMemo} from "react";
 
 const NFT_CONTRACT_ADDRESS = "0x77fedfb705c8bac2e03aad2ad8a8fe83e3e20fa1"
 
 const App = () => {
 
   // Connect to the user's wallet
-  const { provider, address } = useProvider(MetamaskProvider)
+  const {provider} = useProvider(MetamaskProvider)
 
-  provider?.connect()
+  useEffect(() => {
+    const connectProvider = async () => {
+      await provider?.connect()
+    }
+    connectProvider()
+  }, []);
 
-  // Set the price as 0.1 ETH and convert to gwei
+
+  // Set the price as 0.1 ETH and convert to wei
   const priceOfNft = Price.fromRaw('0.01', 18, 'ETH')
 
-  const targetNft = useRef<Target>({
-    // Source chain id (Sepolia in this case)
-    chainId: 11155111,
-    // NFT contract address
-    address: NFT_CONTRACT_ADDRESS,
-    // Recipient wallet address
-    recipient: address!,
-    price: priceOfNft,
-    // The token to swap the payment token to
-    swapTargetTokenSymbol: 'WETH',
-  })
+  const targetNft = useMemo(() => {
+   return {
+     // Source chain id (Sepolia in this case)
+     chainId: 11155111,
+     // NFT contract address
+     address: NFT_CONTRACT_ADDRESS,
+     // Recipient wallet address
+     recipient: provider?.address ?? '',
+     price: priceOfNft,
+     // The token to swap the payment token to
+     swapTargetTokenSymbol: 'WETH',
+   }
+  }, [provider?.address])
 
-  // Encode the transaction
-  const encodedFunctionData = new ethers.utils
-    .Interface(["function buy(address receiver_) payable"])
-    .encodeFunctionData("buy", [
-        address!,
-    ])
+
+  const buildBundle = () => {
+    // Encode the transaction
+    const encodedFunctionData = new ethers.utils.Interface(["function buy(address receiver_) payable"])
+      .encodeFunctionData("buy", [
+        provider?.address!,
+      ])
+
+    console.log(priceOfNft)
+
+    return ethers.utils.defaultAbiCoder.encode(
+      // The Solidity types of the values in the second parameter
+      ["address[]", "uint256[]", "bytes[]"],
+      [
+        [NFT_CONTRACT_ADDRESS],
+        [priceOfNft.value],
+        [encodedFunctionData],
+      ]
+    )
+  }
 
   // Create the transaction bundle
-  const checkoutTxBundle = ethers.utils.defaultAbiCoder.encode(
-    // The Solidity types of the values in the second parameter
-    ["address[]", "uint256[]", "bytes[]"],
-    [
-      [NFT_CONTRACT_ADDRESS],
-      [priceOfNft],
-      [encodedFunctionData],
-    ]
-  )
+  const checkoutTxBundle = useMemo(() => provider?.address ? buildBundle() : '', [provider?.address])
 
   return (
     <div className="app">
-      <DappContextProvider
+      {provider?.address && (<DappContextProvider
         targetNft={targetNft}
         checkoutTxBundle={checkoutTxBundle}
       >
-        <RarimoPayButton />
-      </DappContextProvider>
+        <RarimoPayButton/>
+      </DappContextProvider>)}
     </div>
   )
 }
--- transaction-react/src/index.tsx
+++ transaction-react/src/index.tsx
@@ -1,14 +1,13 @@
-import React from 'react';
-import {createRoot} from 'react-dom/client';
-import './index.css';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
 import App from './App';
 
 const root = createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
+  <StrictMode>
     <App />
-  </React.StrictMode>
+  </StrictMode>
 );
 
