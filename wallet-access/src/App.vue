f8f623c3752106ed87c30c55bf72be4fb968bb67
Tim McMackin <timothymcmackin@yahoo.com>
2023-04-14 15:39:18 -0400

    Trying to print wallet info on the page

--- wallet-access/src/App.vue
+++ wallet-access/src/App.vue
@@ -1,22 +1,28 @@
+<template>
+  <Suspense>
+    <template #fallback>
+      Loading...
+    </template>
+    <template #default>
+      <div>
+        {{ address }}
+      </div>
+    </template>
+  </Suspense>
+</template>
+
 <script setup lang="ts">
 import { createProvider, ChainTypes } from '@rarimo/provider'
 import { MetamaskProvider } from '@rarimo/providers-evm'
 
-const getMetamaskWalletInfo = async () => {
-  // Connect to the Metamask wallet in the browser using Web3.js, using the MetamaskProvider interface to limit bundle size.
-  const provider = await createProvider(MetamaskProvider)
-  await provider.connect()
+// Connect to the Metamask wallet in the browser using Web3.js, using the MetamaskProvider interface to limit bundle size.
+const provider = await createProvider(MetamaskProvider)
+await provider.connect()
 
-  // Get the address of the wallet
-  console.log('Address:', provider.address)
-  console.log('chainType:', ChainTypes[provider.chainType!])
-  console.log('providerType:', provider.providerType)
-}
+// Get the address of the wallet and other info
+console.log('Address:', provider.address)
+console.log('chainType:', ChainTypes[provider.chainType!])
+console.log('providerType:', provider.providerType)
 
-getMetamaskWalletInfo()
+const address = provider.address
 </script>
-
-<template>
-  <div>
-  </div>
-</template>
