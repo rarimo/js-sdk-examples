cd2269cb9816c314c3796ddd7acbf0a360ef5e83
Tim McMackin <timothymcmackin@yahoo.com>
2023-04-17 07:54:27 -0400

    Revert "Trying to print wallet info on the page"
    This reverts commit 3420ef2ba0066203202e8372837c7681852c5d1a.

--- wallet-access/src/App.vue
+++ wallet-access/src/App.vue
@@ -1,28 +1,22 @@
-<template>
-  <Suspense>
-    <template #fallback>
-      Loading...
-    </template>
-    <template #default>
-      <div>
-        {{ address }}
-      </div>
-    </template>
-  </Suspense>
-</template>
-
 <script setup lang="ts">
 import { createProvider, ChainTypes } from '@rarimo/provider'
 import { MetamaskProvider } from '@rarimo/providers-evm'
 
-// Connect to the Metamask wallet in the browser using Web3.js, using the MetamaskProvider interface to limit bundle size.
-const provider = await createProvider(MetamaskProvider)
-await provider.connect()
+const getMetamaskWalletInfo = async () => {
+  // Connect to the Metamask wallet in the browser using Web3.js, using the MetamaskProvider interface to limit bundle size.
+  const provider = await createProvider(MetamaskProvider)
+  await provider.connect()
 
-// Get the address of the wallet and other info
-console.log('Address:', provider.address)
-console.log('chainType:', ChainTypes[provider.chainType!])
-console.log('providerType:', provider.providerType)
+  // Get the address of the wallet
+  console.log('Address:', provider.address)
+  console.log('chainType:', ChainTypes[provider.chainType!])
+  console.log('providerType:', provider.providerType)
+}
 
-const address = provider.address
+getMetamaskWalletInfo()
 </script>
+
+<template>
+  <div>
+  </div>
+</template>
